63a41bb434a4d967038bed56d8069501cbc62aa1
idevnerds-usman-mustafa <usmanmustafa@idevnerds.com>
2022-04-08 12:23:36 +0500

    redux react example

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./css/App.css";
+import { useSelector, useDispatch } from "react-redux";
+import {increment, decrement } from "./actions/index";
 
 function App() {
+
+  // Getting state value from redux store
+  const counter = useSelector((state) => state.incDec);
+
+  // Calling dispatch
+  const dispatch =useDispatch();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="title">Redux Example</div>
+      <div className="counter">{counter}</div>
+      <div className="btn-container">
+        <button className="btn1" onClick={() =>{dispatch(increment())}}>
+          +
+        </button>
+        <button className="btn2" onClick={() =>{dispatch(decrement())}}>
+          -
+        </button>
+      </div>
     </div>
   );
 }
